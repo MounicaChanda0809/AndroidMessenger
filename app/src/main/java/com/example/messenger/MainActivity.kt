37c95f336c26558b8f5af60406093b3fe6efdc0e
Mounica Chanda <MC069394@cerner.net>
2020-01-14 11:08:12 -0600

    Added firebase auth and register screen layout modifications

--- app/src/main/java/com/example/messenger/MainActivity.kt
+++ app/src/main/java/com/example/messenger/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.messenger
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Images.Media.getBitmap
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var selectedPhotoUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         registerButton.setOnClickListener {
-            val email  = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
+        performRegister()
+        }
+
+        profilePictureTextView.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
         }
 
         alreadyHavaAnAccountTextView.setOnClickListener {
@@ -23,4 +37,43 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
     }
+
+    @Suppress("DEPRECATION")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 0 && resultCode.equals(Activity.RESULT_OK) && data != null) {
+           // Log.d("*************", "Photo Selected")
+            selectedPhotoUri = data.data
+            val bitmap = getBitmap(contentResolver, selectedPhotoUri)
+            profilePictureTextView.setBackgroundDrawable(BitmapDrawable(bitmap))
+        }
+    }
+
+    private fun performRegister() {
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email and password", Toast.LENGTH_LONG)
+                .show()
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                uploadImageToFirebase()
+            }
+            .addOnFailureListener {
+                Log.d("FIREBASE", "Failed to create user : ${it.message}")
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_LONG)
+                    .show()
+
+            }
+    }
+
+    private fun uploadImageToFirebase() {
+
+    }
 }
+
